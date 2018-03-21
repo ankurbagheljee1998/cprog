30c686396df893a0f4f4c011db438104928a7056
Ankur Singh <ankurbagheljee1998@gmail.com>
2018-03-21 19:49:18 +0530

    Sample Programs

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Hello World\n";
+	int a=2.9;
+	cout<<"a="<<a<<endl;
+	char name[]="Ankur";
+	cout<<name<<endl;
+	char b[]={'a','b'};
+	cout<<b<<endl;
+	return 0;
+}
